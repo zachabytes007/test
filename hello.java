31f86939707578b01fe1d84271b5b7c2a0addc84
zach <zacharyhz01@gmail.com>
2021-03-23 13:42:30 -0500

    making some changes

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 	public static void main(Sring agrs[]){
 		System.out.println("Hello World!");
 		System.out.println("Adding one more line");
+		System.out.println("Making some changes");
 	}
 }
