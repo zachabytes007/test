6897b119c7569ff630ad3ff4cc15b6f7bb3a4931
zach <zacharyhz01@gmail.com>
2021-03-23 13:48:30 -0500

    bug fixed

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello{
 		System.out.println("Hello World!");
 		System.out.println("Adding one more line");
 		System.out.println("Making some changes");
+		System.out.println("Done with the changes");
 	}
 }
